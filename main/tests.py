2006dfc28a4d470e7010f148085268b001bfdf78
virgo38 <for29you@gmail.com>
2018-12-04 18:21:06 +0300

    Перенес все тесты в один файл.

--- main/tests.py
+++ main/tests.py
@@ -1,10 +1,29 @@
 #!-*-coding:utf-8-*-
 import json
 
-from django.test import TestCase, Client
+from django.test import TestCase, Client, override_settings
 from django.urls import reverse
 
 from main.factories import PageFactory, AudioFactory, VideoFactory, TextFactory
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        self.page = PageFactory()
+        self.audio = AudioFactory()
+        video = VideoFactory()
+        text = TextFactory()
+        self.page.content.add(video, self.audio, text)
+
+    def test_smoke(self):
+        for content in self.page.content.all():
+            assert content.counter == 0
+            if hasattr(content, 'audio'):
+                assert content.audio.bitrate == self.audio.bitrate
+
+    def test_increment_counter(self):
+        self.page.increment_counter()
+        assert self.page.content.filter(counter=1).count() == 3
 
 
 class PageViewTestCase(TestCase):
@@ -37,6 +56,7 @@ class PageViewTestCase(TestCase):
         assert expected['title'] == content.title
         assert expected['counter'] == content.counter
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     def test_get_page_detail(self):
         url1 = reverse('api-page-detail', kwargs={'pk': self.page1.pk})
         url2 = reverse('api-page-detail', kwargs={'pk': self.page2.pk})
